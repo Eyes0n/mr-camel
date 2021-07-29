ba5a40e534a80617e4cfd777abdb6045f29732a6
bohyunkang <bbohyun@kakao.com>
2021-07-30 00:34:19 +0900

    [#1] style : 전역 스타일 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import GlobalStyles from "styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { Theme } from "styles/Theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Routes from "Routes";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyles />
+        <ThemeProvider theme={Theme}>
+          <Routes />
+        </ThemeProvider>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/styles/Theme.js
@@ -0,0 +1,9 @@
+export const Theme = {
+  color: {
+    background: "#F5F5F5",
+    borderline: "#C4C4C4",
+    font: "#352A2A",
+    badge: "#2831F1",
+    white: "#FFFCFC",
+  },
+};
