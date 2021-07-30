30ce420470ca6f61fae2dd856adf9e199c419b14
hyunchanpark <eye3570@gmail.com>
2021-07-30 23:01:26 +0900

    [#6] feat : 관심 없는 상품 라우팅 경고 메시지 모달 추가

---
+++ src/components/WaringModal.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class WaringModal extends Component {
+  render() {
+    const { isShow, isShowWarningPopup } = this.props;
+
+    console.log(isShow);
+    return (
+      <Overlay isShow={isShow} onClick={() => isShowWarningPopup(false)}>
+        <p>관심 없는 상품이므로 상세 페이지로 이동할 수 없습니다.</p>
+      </Overlay>
+    );
+  }
+}
+
+const Overlay = styled.div`
+  position: absolute;
+  display: ${({ isShow }) => (isShow ? "block" : "none")};
+  inset: 0;
+  background-color: rgba(0, 0, 0 0.4);
+
+  p {
+  }
+`;
+
+export default WaringModal;
