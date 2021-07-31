1172aa9849ab3ab7730c8346072608d4e046d03f
Namju <skawn11114@gmail.com>
2021-07-31 11:57:21 +0900

    [#5] feat : 로컬 스토리지 clear  함수 추가

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,3 +1,4 @@
 export const getProducts = () => JSON.parse(localStorage.getItem("visitedItem"));
 export const setProducts = (newValue) =>
   localStorage.setItem("visitedItem", JSON.stringify(newValue));
+export const clearProducts = () => localStorage.clear("visitedItem");
